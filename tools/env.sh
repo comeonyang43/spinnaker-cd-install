cd7d3d1acfe0d762009bf674f89c40852e927f0d
comeonyang43 <55642591+comeonyang43@users.noreply.github.com>
2023-06-24 23:38:48 +0800

    Create env.sh

---
+++ tools/env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION="1.28.7"
+DECK_HOST="http://192.168.146.133"
+GATE_HOST="http://192.168.146.133"
+MINIO_EP="http://minio.default.svc.cluster.local.:9000"
+MINIO_ID="AnWFkXScTzQy3vwd"
+MINIO_KEY="fsob5BYU9NGSkvJnDZXn5XeBR09I7sKa"
+IMG_REGISTRY="registry.cn-hangzhou.aliyuncs.com"
+IMG_NS="priv_sync"
+IMG_REGISTRY_USER="comeonyang"
+IMG_REGISTRY_PASS="Kevin@999#"
+
+BOMS_DIR="/root/.hal/"
+BOMS_FILR=".boms"
+KUBE_DIR="/root/.kube/"
+HALY_IMAGE="${IMG_REGISTRY}/${IMG_NS}/halyard:1.45.0"
